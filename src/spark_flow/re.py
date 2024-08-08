bd3a2ab5e48a3b7fec90aae18383e6571b78de26
j25ng <jsan2861@gmail.com>
2024-08-08 14:28:00 +0900

    fix

--- src/spark_flow/re.py
+++ src/spark_flow/re.py
@@ -6,16 +6,16 @@ def re_partition(load_dt, from_path='data/movie/extract'):
     home_dir = os.path.expanduser("~")
     read_path = f'{home_dir}/{from_path}/load_dt={load_dt}'
     write_base = f'{home_dir}/data/movie/repartition'
-    wirte_path = f'{write_base}/load_dt={load_dt}'
+    write_path = f'{write_base}/load_dt={load_dt}'
 
     df = pd.read_parquet(read_path)
     df['load_dt'] = load_dt
-    rm_dir(wirte_path)
+    rm_dir(write_path)
     df.to_parquet(
             write_base, 
             partition_cols=['load_dt','multiMovieYn', 'repNationCd']
             )
-    return len(df), read_path, wirte_path
+    return len(df), read_path, write_path
 
 def rm_dir(dir_path):
     if os.path.exists(dir_path):
